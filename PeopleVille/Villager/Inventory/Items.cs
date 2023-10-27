513ab2bd44da8aa4fca9c999b2b7f7c8218b08f7
casp103a <casp103a@edu.sde.dk>
2023-10-27 11:26:19 +0200

    Added a parent id for item

--- PeopleVille/Villager/Inventory/Items.cs
+++ PeopleVille/Villager/Inventory/Items.cs
@@ -10,18 +10,45 @@ namespace Villager.Inventory
 {
     public class Items : IItem
     {
+        public int ParentID { get; set; }
         public string Name { get; set; }
         public int Value { get; set; }
         public Action UseAction { get; set; }
         public ItemRarityEnum ItemRarity { get; set; }
         public ItemEnums TypeOfItem { get; set; }
 
-        public Items(ItemEnums type,ItemRarityEnum itemRarity)
+        public Items(int parentID,ItemEnums type,ItemRarityEnum itemRarity)
         {
+            ParentID = parentID;
             Name = $"{itemRarity.ToString()} {type.ToString()}";
             ItemRarity = itemRarity;
             TypeOfItem = type;
             Value = RNGGenarator.GetValueForItem((int)itemRarity);
+            UseAction = GetAction(type);
+        }
+
+        public Action GetAction(ItemEnums type)
+        {
+            switch (type)
+            {
+                case ItemEnums.Money:
+                    break;
+                case ItemEnums.Knife: // stabby stabby
+                    break;
+                case ItemEnums.Hat:
+                    break;
+                case ItemEnums.Watch:
+                    break;
+                case ItemEnums.Glasses:
+                    break;
+                case ItemEnums.Pitchfork:
+                    break;
+                case ItemEnums.Book:
+                    break;
+                default:
+                    break;
+            }
+            return UseAction; // not how to use it.
         }
     }
 }
