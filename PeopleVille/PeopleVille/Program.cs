46f95fd861e1f001f237a923739359668d98737c
Nev0ka <nicolai@osberg.info>
2023-10-27 08:14:06 +0200

    Init For peopleVille

---
+++ PeopleVille/PeopleVille/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PeopleVille
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            /* 
+            Generate Villagers 100 - 2000 need to interact with eachother(use console commands? WPF)
+            Villagers spawn with random items
+            Villager names and ages need to be randomized
+            Villagers need random personalities and charater traits
+            Generate random house, stores and other busnisses
+            */
+        }
+    }
+}
