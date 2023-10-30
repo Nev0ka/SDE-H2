8bb1d11c89ba9a2e65b91a6648b5a89d30a67930
Nev0ka <nicolai@osberg.info>
2023-10-30 09:35:55 +0100

    Added Locations

---
+++ PeopleVille/Village/House.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Village
+{
+    public class House : ILocation
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public int OwnerID { get; set; }
+
+        public House(int id, int ownerId, string villagerName, int value) 
+        {
+            ID = id;
+            OwnerID = ownerId;
+            Name = villagerName + "'s house";
+            Value = value;
+        }
+    }
+}
---
+++ PeopleVille/Village/Store.cs
@@ -0,0 +1,31 @@
+﻿using Enums;
+using RNG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Village
+{
+    internal class Store : ILocation
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public Action Buy { get; set; }
+        public List<string> Items { get; set; }
+
+        public Store(int id, string storeName, int value)
+        {
+            ID = id;
+            Name = storeName;
+            Value = value;
+        }
+
+        public void BuyItem(int itemId, int villagerId)
+        {
+            
+        }
+    }
+}
